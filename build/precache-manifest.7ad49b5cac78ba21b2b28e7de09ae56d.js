175144d6769630bd26e9d15cfd1c46353d382220
Leland T. Klauzer <lklauzer@gmail.com>
2021-03-14 16:35:11 -0700

    publish built app to GitHub

---
+++ build/precache-manifest.7ad49b5cac78ba21b2b28e7de09ae56d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815660de4e5d5af1e714b3749d761a2b",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "bc695b8bff41ae1d7834",
+    "url": "/pomodoro/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "223b709c9b9c0b308480",
+    "url": "/pomodoro/static/js/2.dcb25545.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro/static/js/2.dcb25545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc695b8bff41ae1d7834",
+    "url": "/pomodoro/static/js/main.f67d1080.chunk.js"
+  },
+  {
+    "revision": "a9aee71dab78a19abc00",
+    "url": "/pomodoro/static/js/runtime-main.50bf0b17.js"
+  }
+]);
